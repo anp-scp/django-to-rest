e424874b2649843bb520d83b97f04cef8b02eb4f
anp-scp <anupammrg@gmail.com>
2022-10-02 23:01:29 +0530

    Fix issues with many to one; many to many; fixed ObjectNotExist issue...

--- to_rest/views.py
+++ to_rest/views.py
@@ -11,12 +11,29 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from to_rest import constants
 from rest_framework.viewsets import ModelViewSet
 from to_rest import cfg
+from django.shortcuts import get_object_or_404
 
 REST_FRAMEWORK_SETTINGS = None
 try:
     REST_FRAMEWORK_SETTINGS = settings.REST_FRAMEWORK
 except Exception:
     pass
+
+def listMethodFactory(modelName):
+    def list(self, request, *args, **kwargs):
+        objects = self.filter_queryset(self.get_queryset())
+        if len(objects) == 0:
+            headers = {}
+            headers[constants.CONTENT_MESSAGE] = constants.NO_OBJECT_EXISTS.format("related "+modelName)
+            return Response(status=status.HTTP_204_NO_CONTENT, headers=headers)
+        else:
+            page = self.paginate_queryset(objects)
+            if page is not None:
+                serializer =  self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+            serializer = self.get_serializer(objects, many=True)
+            return Response(serializer.data)
+    return list
 
 def isDefaultSerializer(serializer):
     """
@@ -31,7 +48,7 @@ def isDefaultSerializer(serializer):
     return serializer.__name__.startswith(constants.PROJECT_NAME_PREFIX)
 
 
-def getTempViewSet(queryset, childModel, childSerializer, viewParams):
+def getTempViewSet(queryset, childModel, childSerializer, viewParams, extras):
     """
     Function to create temporary views for filtering and permission purposes for actions as
     actions do not accept additional filterset_class or filterset_fields.
@@ -64,6 +81,22 @@ def getTempViewSet(queryset, childModel, childSerializer, viewParams):
     if attributes.get(constants.FILTERSET_CLASS, False):
         if attributes.get(constants.FILTERSET_FIELDS, False):
             del attributes[constants.FILTERSET_FIELDS]#remove filterset_filds if filterset_class exists
+    
+    if extras is not None and extras.get('create', False):
+        if not attributes.get('create', False):
+            attributes['create'] = extras['create']
+    
+    if extras is not None and extras.get('list', False):
+        if not attributes.get('list', False):
+            attributes['list'] = extras['list']
+    
+    if extras is not None and extras.get('update', False):
+        if not attributes.get('update', False):
+            attributes['update'] = extras['update']
+    
+    if extras is not None and extras.get('get_object', False):
+        if not attributes.get('get_object', False):
+            attributes['get_object'] = extras['get_object']
 
     return type(constants.PROJECT_NAME_PREFIX + "temp_" + childModel._meta.label.replace('.','_'), (ModelViewSet,), attributes)
 
@@ -102,9 +135,9 @@ def oneToManyActionFactory(parentModel,childSerializer, field, relatedName):
 
 
     def funcRelatedList(self,request,pk=None, *args, **kwargs):
-        parentObject = parentModel.objects.get(pk=pk)
+        parentObject = get_object_or_404(parentModel, pk=pk)
         childObjects = eval("parentObject.{}.all()".format(relatedName))
-        tempViewSet = getTempViewSet(childObjects, childModel, childSerializer, childCustomViewParams)
+        tempViewSet = getTempViewSet(childObjects, childModel, childSerializer, childCustomViewParams, {'list': listMethodFactory(childModel.__name__)})
         tempView = tempViewSet.as_view({'get': 'list'})
         return tempView(request._request,*args,**kwargs)
 
@@ -154,45 +187,37 @@ def manyToManyActionFactory(parentModel, field, relatedName):
     if cfg.djangoToRestRegistry.get(throughModel.__name__, False):
         if cfg.djangoToRestRegistry[throughModel.__name__].get(constants.CUSTOM_VIEW_PARAMS, False):
             viewParams = cfg.djangoToRestRegistry[throughModel.__name__][constants.CUSTOM_VIEW_PARAMS]
-    
-    def funcRelatedList(self,request,pk=None):
+
+    def funcRelatedList(self,request,pk=None, *args,**kwargs):
         #Kind of list view
         if self.request.method == "GET":
             #The listing will be of the through objects as it present more information about the relation when there
             # are additional information.
-            parentObject = parentModel.objects.get(pk=pk)
+            parentObject = get_object_or_404(parentModel, pk=pk) #just to raise 404
             filter_param = field.field.m2m_reverse_field_name() + "_" + field.field.m2m_reverse_target_field_name() if isinstance(field,ManyToManyRel) else field.m2m_field_name() + "_" + field.m2m_target_field_name()
-            throughObjects = eval("parentObject.{}.through.objects.filter({}=pk)".format(relatedName, filter_param))
-            tempView = getTempViewSet(throughObjects, throughModel, throughSerializer, viewParams)
-            tempViewObj = tempView()
-            try:
-                for backend in list(getattr(tempViewObj, constants.FILTER_BACKENDS)):
-                    throughObjects = backend().filter_queryset(self.request, throughObjects, tempViewObj)
-            except Exception as e:
-                traceback.print_exc()
-            if len(throughObjects) == 0:
-                headers = {}
-                headers[constants.CONTENT_MESSAGE] = constants.NO_OBJECT_EXISTS.format("related "+throughModel.__name__)
-                return Response(status=status.HTTP_204_NO_CONTENT, headers=headers)
-            else:
-                page = self.paginate_queryset(throughObjects)
-                if page is not None:
-                    serializer =  throughSerializer(page, many=True)
-                    return self.get_paginated_response(serializer.data)
-                serializer = throughSerializer(throughObjects, many=True)
-                return Response(serializer.data)
+            throughObjects = eval("throughModel.objects.filter({}=pk)".format(filter_param))
+            tempViewSet = getTempViewSet(throughObjects, throughModel, throughSerializer, viewParams, {'list': listMethodFactory(throughModelName)})
+            tempView = tempViewSet.as_view({'get': 'list'})
+            return tempView(request._request,*args,**kwargs)
+            
         elif self.request.method == "POST":
             #To create new relation ship, through objects would be used as it contains prmary key
             #of both sides of the relationship
-            parentObject = parentModel.objects.get(pk=pk)
+            parentObject = get_object_or_404(parentModel, pk=pk) #just to raise 404
             providedData = request.data
             parentObjectField = field.field.m2m_reverse_field_name()  if isinstance(field,ManyToManyRel) else field.m2m_field_name()
             providedData[parentObjectField] = pk
-            serializer = throughSerializer(data=providedData)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+            request._full_data = providedData
+            throughObjects = parentModel.objects.all()
+            def create(self, request, *args, **kwargs):
+                serializer = self.get_serializer(data=providedData)
+                serializer.is_valid(raise_exception=True)
+                self.perform_create(serializer)
+                headers = self.get_success_headers(serializer.data)
+                return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+            tempViewSet = getTempViewSet(throughObjects, throughModel, throughSerializer, viewParams, {'create': create})
+            tempView = tempViewSet.as_view({'post': 'create'})
+            return tempView(request._request, *args, **kwargs)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
@@ -200,45 +225,78 @@ def manyToManyActionFactory(parentModel, field, relatedName):
     funcRelatedList.__name__ = constants.MANY_TO_MANY_LIST_ACTION + relatedName
     funcRelatedList = action(detail=True, methods=['get', 'post'], url_path=relatedName, url_name=parentModelName.lower() + "-" + relatedName +"-list")(funcRelatedList)
 
-    def funcRelatedDetail(self,request,childPk,pk=None):
+    def funcRelatedDetail(self,request,childPk,pk=None,*args,**kwargs):
         #kind of detail view
         if self.request.method == "PUT":
             #For, updating a relationship, through object will be used. For, that reason, the primary
             #key of the through object will be used as path parameter in the nested url. To get the primary
             #key of the through object filter can be used in with the url of list view (GET)
-            parentObject = parentModel.objects.get(pk=pk)
-            filter_param = field.field.m2m_field_name() + "_" + field.field.m2m_target_field_name() if isinstance(field,ManyToManyRel) else field.m2m_reverse_field_name() + "_" + field.m2m_reverse_target_field_name()
+            parentObject = get_object_or_404(parentModel, pk=pk) #just to raise 404
             parentObjectField = field.field.m2m_reverse_field_name()  if isinstance(field,ManyToManyRel) else field.m2m_field_name()
-            throughObject = eval("parentObject.{}.through.objects.get(pk=childPk)".format(relatedName))
+            throughObject = get_object_or_404(throughModel, pk=childPk)
             providedData = request.data
             providedData[parentObjectField] = pk
-            serializer = throughSerializer(throughObject, data=providedData, partial=False)
-            serializer.is_valid(raise_exception=True)
-            self.perform_update(serializer)
-            return Response(serializer.data)
+            def get_object(self):
+                return throughObject
+            def update(self, request, *args, **kwargs):
+                partial = kwargs.pop('partial', False)
+                instance = self.get_object()
+                serializer = self.get_serializer(instance, data=providedData, partial=partial)
+                serializer.is_valid(raise_exception=True)
+                self.perform_update(serializer)
+
+                if getattr(instance, '_prefetched_objects_cache', None):
+                    # If 'prefetch_related' has been applied to a queryset, we need to
+                    # forcibly invalidate the prefetch cache on the instance.
+                    instance._prefetched_objects_cache = {}
+
+                return Response(serializer.data)
+            throughObjects = throughModel.objects.all()
+            tempViewSet = getTempViewSet(throughObjects, throughModel, throughSerializer, viewParams, {'update': update, 'get_object': get_object})
+            tempView = tempViewSet.as_view({'put': 'update'})
+            return tempView(request._request, *args, **kwargs)
         elif self.request.method == "PATCH":
             #For, updating a relationship, through object will be used. For, that reason, the primary
             #key of the through object will be used as path parameter in the nested url. To get the primary
             #key of the through object filter can be used in with the url of list view (GET)
-            parentObject = parentModel.objects.get(pk=pk)
-            filter_param = field.field.m2m_field_name() + "_" + field.field.m2m_target_field_name() if isinstance(field,ManyToManyRel) else field.m2m_reverse_field_name() + "_" + field.m2m_reverse_target_field_name()
+            parentObject = get_object_or_404(parentModel, pk=pk) #just to raise 404
             parentObjectField = field.field.m2m_reverse_field_name()  if isinstance(field,ManyToManyRel) else field.m2m_field_name()
-            throughObject = eval("parentObject.{}.through.objects.get(pk=childPk)".format(relatedName))
+            throughObject = get_object_or_404(throughModel, pk=childPk)
             providedData = request.data
             providedData[parentObjectField] = pk
-            serializer = throughSerializer(throughObject, data=providedData, partial=True)
-            serializer.is_valid(raise_exception=True)
-            self.perform_update(serializer)
-            return Response(serializer.data)
+            def get_object(self):
+                return throughObject
+            def update(self, request, *args, **kwargs):
+                partial = kwargs.pop('partial', False)
+                instance = self.get_object()
+                serializer = self.get_serializer(instance, data=providedData, partial=partial)
+                serializer.is_valid(raise_exception=True)
+                self.perform_update(serializer)
+
+                if getattr(instance, '_prefetched_objects_cache', None):
+                    # If 'prefetch_related' has been applied to a queryset, we need to
+                    # forcibly invalidate the prefetch cache on the instance.
+                    instance._prefetched_objects_cache = {}
+
+                return Response(serializer.data)
+            throughObjects = throughModel.objects.all()
+            tempViewSet = getTempViewSet(throughObjects, throughModel, throughSerializer, viewParams, {'update': update, 'get_object': get_object})
+            tempView = tempViewSet.as_view({'patch': 'partial_update'})
+            return tempView(request._request, *args, **kwargs)
         elif self.request.method == "DELETE":
             #For, deleting a relationship, through object will be used. For, that reason, the primary
             #key of the through object will be used as path parameter in the nested url. To get the primary
             #key of the through object filter can be used in with the url of list view (GET)
-            parentObject = parentModel.objects.get(pk=pk)
-            filter_param = field.field.m2m_field_name() + "_" + field.field.m2m_target_field_name() if isinstance(field,ManyToManyRel) else field.m2m_reverse_field_name() + "_" + field.m2m_reverse_target_field_name()
-            throughObject = eval("parentObject.{}.through.objects.get(pk=childPk)".format(relatedName))
-            self.perform_destroy(throughObject)
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            parentObject = get_object_or_404(parentModel, pk=pk) #just to raise 404
+            throughObject = get_object_or_404(throughModel, pk=childPk)
+            def get_object(self):
+                return throughObject
+            throughObjects = throughModel.objects.all()
+            tempViewSet = getTempViewSet(throughObjects, throughModel, throughSerializer, viewParams, {'get_object': get_object})
+            tempView = tempViewSet.as_view({'delete': 'destroy'})
+            return tempView(request._request, *args, **kwargs)
+            # self.perform_destroy(throughObject)
+            # return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
     
